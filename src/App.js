d40ae88e10336b15be9cf5473b778e5d7b47e770
Dipam Patel <pateldipam6653@gmail.com>
2022-03-14 00:51:36 +0530

    Updated Dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [mode,setMode] = useState('light')
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  } 
+
   return (
     <div>
-      <Navbar title="LiveCricketScore" link1="https://reactjs.org/docs/introducing-jsx.html"/>
+      <Navbar title="TextUtils" link1="https://reactjs.org/docs/introducing-jsx.html" mode={mode} toggleMode={toggleMode}/>
 
       <div className="container">
-        <TextForm heading="Enter the text to analyze:"/>
+        <TextForm heading="Enter the text to analyze:" mode={mode}/>
       </div>
       
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,16 +2,7 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
-    const [text, setText] = useState("Enter Text Here");
-
-    const [myStyle, setMyStyle] = useState({
-                                    color: 'black',
-                                    backgroundColor: 'white',
-                                    border: '4px solid grey',
-                                    height: 180,
-                                 });
-
-    const [btnText, setBtnText] = useState("Dark-Mode on");   
+    const [text, setText] = useState("");
     
     const [currentTextSize, setCurrentTextSize] = useState(15); 
 
@@ -38,29 +29,6 @@ export default function TextForm(props) {
         setText(lowerText)
     } 
 
-    const handleDarkMode = (event)=>{
-        if(myStyle.color ==='white'){
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white',
-                border: '4px solid grey', 
-                fontSize: currentTextSize+'px',
-                height: 180,
-            })
-            setBtnText("Dark-Mode on")
-        }
-        else{
-            setMyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                border: '4px solid grey',
-                fontSize: currentTextSize+'px',
-                height: 180,
-            })
-            setBtnText("Dark-Mode off")
-        }
-    } 
-
     const handleCopy = (event)=>{
         let tempTxt = document.getElementById("myBox")
         tempTxt.select()
@@ -75,48 +43,35 @@ export default function TextForm(props) {
     const handleRange = (event)=>{
         let newTextSize = document.getElementById("customRange1").value
         setCurrentTextSize(newTextSize)
-        if(myStyle.backgroundColor === 'white'){
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white',
-                border: '4px solid grey',
-                fontSize: currentTextSize+'px',
-                height: 180,
-            })
-        }
-        else{
-            setMyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                border: '4px solid grey',
-                fontSize: currentTextSize+'px',
-                height: 180,
-            })
-        }
     } 
 
   return (
     <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode==='light'?'black':'white'}}>
             <h2 className='my-4'>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" value={text} style={myStyle} onChange={handleOnChange} rows="5"></textarea>
+                <textarea className="form-control" id="myBox" value={text} 
+                                                            style={{color: props.mode==='light'?'black':'white',
+                                                                backgroundColor: props.mode==='light'?'white':'grey',
+                                                                border: props.mode==='light'? '4px solid grey':'4px solid wheat',
+                                                                fontSize: currentTextSize+'px',
+                                                                height: 180,
+                                                            }} onChange={handleOnChange} rows="5"></textarea>
             </div>
             <button className="btn btn-primary my-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-danger mx-2 my-1" onClick={handlelowerClick}>Convert to Lowercase</button> 
             <button className="btn btn-success mx-2 my-1" onClick={handleEmptyClick}>Clear TextArea</button>
             <button className="btn btn-warning mx-2 my-1" onClick={handleRevsrseClick}>Inverse Text</button>
-            <button className="btn btn-dark mx-0 my-1" onClick={handleDarkMode}>{btnText}</button>
             <button className="btn btn-info mx-2 my-1" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-secondary mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             <input type="range" className="form-range-track-width: 15% my-1" value={currentTextSize} onChange={handleRange} min="8" id="customRange1"/>
         </div>
-        <div className='container'>
+        <div className='container' style={{color: props.mode==='light'?'black':'white'}}>
             <h2 className='my-4'>Your Text Summary: </h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>Average Time required to read text is {text.split(" ").length * 0.008} minutes</p>
             <h3>Preview</h3>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter Something in above text-box to preview it here."}</p>
         </div>
     </>
   )
